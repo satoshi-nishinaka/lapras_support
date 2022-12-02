2b9aba2c5ea571eee8a1f75b6edf4d3a27faa72f
Satoshi Nishinaka <satoshie.sp@gmail.com>
2022-12-02 23:08:12 +0900

    ショートカットキーが機能していなかったので修正

--- src/background.ts
+++ src/background.ts
@@ -2,47 +2,49 @@ import { Storage } from './Storage';
 
 const storage = new Storage();
 
+storage.load(() => {
 // ショートカットキーのハンドリング
-chrome.commands.onCommand.addListener((command) => {
-  function getCandidateIds(storage: Storage) {
-    const id = storage.candidateHighIds.shift();
-    if (!id) {
-      return storage.candidateLowIds.shift();
+  chrome.commands.onCommand.addListener((command) => {
+    function getCandidateIds(storage: Storage) {
+      const id = storage.candidateHighIds.shift();
+      if (!id) {
+        return storage.candidateLowIds.shift();
+      }
+      return id;
     }
-    return id;
-  }
 
-  switch (command) {
-    case 'redirect_to_next_candidate': {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const currentTab = tabs[0];
-        const url = currentTab.url;
-        console.log(url);
-        // Laprasのページを開いている場合は保存してあるCandidateIdsを元にページを開く
-        if (url.startsWith('https://scout.lapras.com/')) {
-          const id = getCandidateIds(storage);
-          console.log(id);
-          if (!id) {
-            if (confirm('プールされているIDがありません')) {
-              storage.save(() => {
-                chrome.tabs.update(currentTab.id, {
-                  url: 'https://scout.lapras.com/talent_pool/',
+    switch (command) {
+      case 'redirect_to_next_candidate': {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+          const currentTab = tabs[0];
+          const url = currentTab.url;
+          console.log(url);
+          // Laprasのページを開いている場合は保存してあるCandidateIdsを元にページを開く
+          if (url.startsWith('https://scout.lapras.com/')) {
+            const id = getCandidateIds(storage);
+            console.log(id);
+            if (!id) {
+              if (confirm('プールされているIDがありません')) {
+                storage.save(() => {
+                  chrome.tabs.update(currentTab.id, {
+                    url: 'https://scout.lapras.com/talent_pool/',
+                  });
                 });
-              });
 
-              return;
+                return;
+              }
             }
-          }
-          storage.checkedCandidateIds.push(id);
-          storage.save(() => {
-            chrome.tabs.update(currentTab.id, {
-              url: `https://scout.lapras.com/talent_pool/candidates/${id}`,
+            storage.checkedCandidateIds.push(id);
+            storage.save(() => {
+              chrome.tabs.update(currentTab.id, {
+                url: `https://scout.lapras.com/talent_pool/candidates/${id}`,
+              });
             });
-          });
-          return;
-        }
-      });
-      break;
+            return;
+          }
+        });
+        break;
+      }
     }
-  }
+  });
 });
