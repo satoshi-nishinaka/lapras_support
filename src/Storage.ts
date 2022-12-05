407d092fab0d74d847444853c0789d049ca12b77
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-12-05 19:35:46 +0900

    かなり強引だがリストとして持たせられる件数を増やした

--- src/Storage.ts
+++ src/Storage.ts
@@ -6,19 +6,46 @@ export class Storage {
   public loadDelay = 4000;
 
   load(callBack?: () => void): void {
+    // candidateIdはそれぞれ271件までしか保存できないっぽい
     const values = [
       'enablePooling',
-      'candidateHighIds',
-      'candidateLowIds',
-      'checkedCandidateIds',
+      'candidateHighIds0',
+      'candidateHighIds1',
+      'candidateHighIds2',
+      'candidateHighIds3',
+      'candidateLowIds0',
+      'candidateLowIds1',
+      'candidateLowIds2',
+      'candidateLowIds3',
+      'checkedCandidateIds0',
+      'checkedCandidateIds1',
+      'checkedCandidateIds2',
+      'checkedCandidateIds3',
       'loadDelay',
     ];
     chrome.storage.local.get(values, (items) => {
       // LocalStorageから設定情報を取得
+      console.debug('load storage values ----');
+      console.debug(items);
       this.enablePooling = items.enablePooling ?? true;
-      this.candidateHighIds = items.candidateHighIds ?? [];
-      this.candidateLowIds = items.candidateLowIds ?? [];
-      this.checkedCandidateIds = items.checkedCandidateIds ?? [];
+      this.candidateHighIds = [].concat(
+        items.candidateHighIds0 ?? [],
+        items.candidateHighIds1 ?? [],
+        items.candidateHighIds2 ?? [],
+        items.candidateHighIds3 ?? []
+      );
+      this.candidateLowIds = [].concat(
+        items.candidateLowIds0 ?? [],
+        items.candidateLowIds1 ?? [],
+        items.candidateLowIds2 ?? [],
+        items.candidateLowIds3 ?? []
+      );
+      this.checkedCandidateIds = [].concat(
+        items.checkedCandidateIds0 ?? [],
+        items.checkedCandidateIds1 ?? [],
+        items.checkedCandidateIds2 ?? [],
+        items.checkedCandidateIds3 ?? []
+      );
       this.loadDelay = items.loadDelay ?? 4000;
 
       if (callBack) {
@@ -31,11 +58,39 @@ export class Storage {
    * ローカルストレージに設定内容を保存します
    */
   save(callback?: () => void): void {
+    const candidateHighIds = [
+      this.candidateHighIds.slice(0, 200),
+      this.candidateHighIds.slice(200, 400),
+      this.candidateHighIds.slice(400, 600),
+      this.candidateHighIds.slice(600, 800),
+    ];
+    const candidateLowIds = [
+      this.candidateLowIds.slice(0, 200),
+      this.candidateLowIds.slice(200, 400),
+      this.candidateLowIds.slice(400, 600),
+      this.candidateLowIds.slice(600, 800),
+    ];
+    const checkedCandidateIds = [
+      this.checkedCandidateIds.slice(0, 200),
+      this.checkedCandidateIds.slice(200, 400),
+      this.checkedCandidateIds.slice(400, 600),
+      this.checkedCandidateIds.slice(600, 800),
+    ];
+
     const values = {
       enablePooling: this.enablePooling,
-      candidateHighIds: this.candidateHighIds,
-      candidateLowIds: this.candidateLowIds,
-      checkedCandidateIds: this.checkedCandidateIds,
+      candidateHighIds0: candidateHighIds[0],
+      candidateHighIds1: candidateHighIds[1],
+      candidateHighIds2: candidateHighIds[2],
+      candidateHighIds3: candidateHighIds[3],
+      candidateLowIds0: candidateLowIds[0],
+      candidateLowIds1: candidateLowIds[1],
+      candidateLowIds2: candidateLowIds[2],
+      candidateLowIds3: candidateLowIds[3],
+      checkedCandidateIds0: checkedCandidateIds[0],
+      checkedCandidateIds1: checkedCandidateIds[1],
+      checkedCandidateIds2: checkedCandidateIds[2],
+      checkedCandidateIds3: checkedCandidateIds[3],
       loadDelay: this.loadDelay,
     };
     console.log('saveValuesForLocalStorage -----');
